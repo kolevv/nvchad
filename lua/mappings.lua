577ad5ca213a54ac6d3d3a56954d8635a2627aba
Viktor Kolev <kolevv@protonmail.com>
2026-05-19 14:34:53 +0300

    Add gitsigns connfig + keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,13 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+-- LSP
 map("", "<leader>fr", "<cmd>Telescope lsp_references<CR>", { desc = "Find all references" })
 map("n", "<leader>ww", vim.lsp.buf.code_action, { desc = "Code Actions" })
+
+-- GitSigns
+map("", "<leader>gy", "<cmd>Gitsigns blame_line<CR>", { desc = "Show blame information for current line in popup" })
+map("", "<leader>gd", "<cmd>Gitsigns toggle_word_diff<CR>", { desc = "Show intra-line word-diff" })
+map("", "<leader>gb", "<cmd>Gitsigns blame<CR>", { desc = "Show blame of current buffer" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
